23044b85cc178e5e429cd92da981f58ae5e9e9f2
Crowdbotics <team@crowdbotics.com>
2023-05-08 16:20:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-credit-40549",
+  url: "https://summer-credit-40549.botics.co",
+  api: "https://summer-credit-40549.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
